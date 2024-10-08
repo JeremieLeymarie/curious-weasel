e7e08568626ea30dbc6a56c63e366500fd03b285
JeremieLeymarie <jeremie.leymarie@napta.io>
2024-10-08 15:48:34 +0200

    :bug: Fix get_env with integer default value

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,7 @@ config :time_tracker, TimeTracker.Repo,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10,
-  port: System.get_env("PGPORT", 5432)
+  port: System.get_env("PGPORT", "5432")
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
