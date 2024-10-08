669db3c97faf9d585b2d1188699ab81d1ee8b7b3
JeremieLeymarie <jeremie.leymarie@napta.io>
2024-10-08 14:08:47 +0200

    :sparkles: Add working times and clocks to user endpoint

--- lib/time_tracker_web/controllers/user_json.ex
+++ lib/time_tracker_web/controllers/user_json.ex
@@ -16,10 +16,32 @@ defmodule TimeTrackerWeb.UserJSON do
   end
 
   defp data(%User{} = user) do
-    %{
+    result = %{
       id: user.id,
       username: user.username,
-      email: user.email
+      email: user.email,
+      clocks: nil,
+      working_times: nil
     }
+
+    clocks =
+      if Ecto.assoc_loaded?(user.clocks) do
+        for(clock <- user.clocks, do: %{id: clock.id, time: clock.time, status: clock.status})
+      else
+        nil
+      end
+
+    working_times =
+      if Ecto.assoc_loaded?(user.working_times) do
+        for(
+          working_time <- user.working_times,
+          do: %{id: working_time.id, start: working_time.start, end: working_time.end}
+        )
+      else
+        nil
+      end
+
+    result = %{result | clocks: clocks}
+    %{result | working_times: working_times}
   end
 end
