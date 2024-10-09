8667d24062758ff8f8b0fc9ffd5ff97fefd007c1
DylanWeinachter <dylan.weinachter@gmail.com>
2024-10-09 14:59:59 +0200

    :ambulance: :ambulance: :ambulance: - Adding WorkingTimes Vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,19 @@
-<script setup lang="ts"></script>
-
 <template>
-  <header class="flex gap-8 items-center">
-    <img alt="Vue logo" class="logo" src="./assets/Logo_nobg.png" width="100" height="125" />
-    <div class="space-x-2">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/user">User</RouterLink>
-    </div>
-  </header>
-  <main>
-    <RouterView :key="$route.path" />
-  </main>
+  <div class="min-h-screen h-full w-full bg-gray-100 flex items-center justify-center">
+    <WorkingTimes :userId="2" />
+  </div>
 </template>
+
+<script>
+import WorkingTimes from './components/WorkingTimes.vue';
+
+export default {
+  components: {
+    WorkingTimes,
+  },
+};
+</script>
+
+<style>
+/* Ajoutez des styles globaux si nécessaire */
+</style>
