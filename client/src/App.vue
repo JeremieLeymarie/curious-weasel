39e61d9fb0e0ecc985fb81beb225114b16af3acc
Guillaum-th <guillaume.thiebaud@epitech.eu>
2024-10-09 15:18:46 +0200

    add routing for worktime

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="min-h-screen h-full w-full bg-gray-100 flex items-center justify-center">
-    <WorkingTimes :userId="2" />
-  </div>
   <header class="flex gap-8 items-center">
     <img alt="Vue logo" class="logo" src="./assets/Logo_nobg.png" width="100" height="125" />
     <div class="space-x-2">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/user/1">User</RouterLink>
+      <RouterLink to="/workingtimecreated">Working Time created</RouterLink>
+      <RouterLink to="/workingtimeupdate">Working Time updated</RouterLink>
       <RouterLink to="/workingtime">Working Time</RouterLink>
-      <RouterLink to="/workingtimeupdate">Working Time</RouterLink>
     </div>
   </header>
   <main class="p-4">
@@ -16,15 +14,7 @@
   </main>
 </template>
 
-<script>
-import WorkingTimes from './components/WorkingTimes.vue';
-
-export default {
-  components: {
-    WorkingTimes,
-  },
-};
-</script>
+<script lang="ts"></script>
 
 <style>
 /* Ajoutez des styles globaux si nécessaire */
--- client/src/router.ts
+++ client/src/router.ts
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import UserComponent from './components/UserComponent.vue'
 import NewUser from './components/NewUser.vue'
 import HomeComponent from './components/HomeComponent.vue'
-import WorkingTimes from './components/WorkingTime.vue'
+import WorkingTimesCreated from './components/WorkingTime.vue'
 import WorkingTimesUpdate from './components/WorkingTimeUpdate.vue'
+import WorkingTimes from './components/WorkingTimes.vue'
 
 const routes = [
   { path: '/', component: HomeComponent },
+  { path: '/workingtimecreated', component: WorkingTimesCreated },
   { path: '/workingtime', component: WorkingTimes },
   { path: '/workingtimeupdate', component: WorkingTimesUpdate },
   { path: '/user/:id', component: UserComponent },
