b5f8ac3df416820bf123bc8f95664ddba67b54d6
JeremieLeymarie <jeremie.leymarie@napta.io>
2024-10-09 16:31:02 +0200

    :recycle: Refacto working times vue

--- client/src/components/WorkingTimes.vue
+++ client/src/components/WorkingTimes.vue
@@ -1,7 +1,114 @@
+<!-- TODO: fix Typescript errors -->
+
+<script setup lang="ts">
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+import { computed, onMounted, ref } from 'vue'
+import type { WorkingTime } from '@/types'
+const router = useRouter()
+
+const data = ref<{ workingTimes: WorkingTime[]; loading: boolean; expandedDate: Date | null }>({
+  workingTimes: [],
+  loading: true,
+  expandedDate: null
+})
+
+const deleteWorkingTime = async (id: string) => {
+  try {
+    let res = await fetch('http://localhost:4000/api/workingtimes/' + id, {
+      method: 'DELETE'
+    })
+    if (res.status == 204) {
+      console.log('ok')
+    } else {
+      console.log('ko')
+    }
+  } catch (error) {
+    console.error('Error fetching working times:', error)
+  }
+}
+
+const updateWorkingTime = async (id: string, start: Date, end: Date) => {
+  try {
+    console.log(router)
+    router.push(`/workingtimeupdate/${id}/${start}/${end}`)
+  } catch (error) {
+    console.error('Error fetching working times:', error)
+  }
+}
+
+const getWorkingTimes = async () => {
+  try {
+    const response = await axios.get(`http://localhost:4000/api/workingtimes/` + 1)
+    data.value.workingTimes = response.data.data
+    data.value.loading = false
+  } catch (error) {
+    console.error('Error fetching working times:', error)
+  }
+}
+const formatTime = (dateString: string) => {
+  const date = new Date(dateString)
+  return (
+    date.getUTCHours().toString().padStart(2, '0') +
+    ':' +
+    date.getUTCMinutes().toString().padStart(2, '0')
+  )
+}
+
+const calculateDuration = (start: Date, end: Date) => {
+  const startDate = new Date(start)
+  const endDate = new Date(end)
+  const duration = (endDate - startDate) / (1000 * 60 * 60) // Duration in hours
+  return duration.toFixed(2)
+}
+
+const calculateTotalDuration = (times: any[]) => {
+  return times
+    .reduce((total, time) => {
+      const startDate = new Date(time.start)
+      const endDate = new Date(time.end)
+      return total + (endDate - startDate) / (1000 * 60 * 60)
+    }, 0)
+    .toFixed(2)
+}
+const getPeriod = (start: string) => {
+  const hour = new Date(start).getHours()
+  return hour < 12 ? 'Morning' : 'Afternoon'
+}
+
+const toggleDetails = (date: Date) => {
+  data.value.expandedDate = data.value.expandedDate === date ? null : date
+}
+
+const groupedWorkingTimes = computed(() => {
+  const grouped = {}
+  data.value.workingTimes.forEach((time) => {
+    const dateKey = new Date(time.start).toLocaleDateString()
+    if (!grouped[dateKey]) {
+      grouped[dateKey] = []
+    }
+    grouped[dateKey].push(time)
+  })
+
+  const sortedKeys = Object.keys(grouped).sort((a, b) => new Date(a) - new Date(b))
+
+  const sortedGrouped = {}
+  sortedKeys.forEach((key) => {
+    sortedGrouped[key] = grouped[key]
+  })
+
+  return sortedGrouped
+})
+
+onMounted(() => {
+  getWorkingTimes()
+})
+</script>
+
 <template>
   <div class="p-6 bg-gray-800 shadow-lg rounded-lg w-full">
     <h2 class="text-2xl font-bold mb-6 text-white">Working Times for User {{ userId }}</h2>
-    <div v-if="loading" class="text-center text-gray-300">Loading...</div>
+    <div v-if="data.loading" class="text-center text-gray-300">Loading...</div>
     <div v-else>
       <ul>
         <li v-for="(times, date) in groupedWorkingTimes" :key="date" class="mb-4">
@@ -14,7 +121,7 @@
               <span>{{ calculateTotalDuration(times) }} hours</span>
             </div>
           </div>
-          <div v-if="expandedDate === date" class="mt-2 p-4 bg-gray-600 text-white rounded">
+          <div v-if="data.expandedDate === date" class="mt-2 p-4 bg-gray-600 text-white rounded">
             <div v-for="(time, index) in times" :key="index" class="mb-2">
               <p>
                 <strong>{{ getPeriod(time.start) }}:</strong> {{ formatTime(time.start) }} -
@@ -40,120 +147,3 @@
     </div>
   </div>
 </template>
-
-<script>
-import axios from 'axios'
-import { useRoute, useRouter } from 'vue-router'
-import { ref } from 'vue'
-const router = ref(useRouter())
-
-export default {
-  props: {
-    userId: {
-      type: Number,
-      required: true
-    }
-  },
-  data() {
-    return {
-      workingTimes: [],
-      loading: true,
-      expandedDate: null
-    }
-  },
-  computed: {
-    groupedWorkingTimes() {
-      // Crée un objet pour grouper les working times par date
-      const grouped = {}
-      this.workingTimes.forEach((time) => {
-        const dateKey = new Date(time.start).toLocaleDateString()
-        if (!grouped[dateKey]) {
-          grouped[dateKey] = []
-        }
-        grouped[dateKey].push(time)
-      })
-
-      // Trie les dates du plus ancien au plus récent
-      const sortedKeys = Object.keys(grouped).sort((a, b) => new Date(a) - new Date(b))
-
-      // Retourne un nouvel objet trié par date
-      const sortedGrouped = {}
-      sortedKeys.forEach((key) => {
-        sortedGrouped[key] = grouped[key]
-      })
-
-      return sortedGrouped
-    }
-  },
-  methods: {
-    async deleteWorkingTime(id) {
-      try {
-        let res = await fetch('http://localhost:4000/api/workingtimes/' + id, {
-          method: 'DELETE'
-        })
-        if (res.status == 204) {
-          console.log('ok')
-        } else {
-          console.log('ko')
-        }
-      } catch (error) {
-        console.error('Error fetching working times:', error)
-      }
-    },
-    async updateWorkingTime(id, start, end) {
-      try {
-        console.log(router)
-        router.push(`/workingtimeupdate/${id}/${start}/${end}`)
-      } catch (error) {
-        console.error('Error fetching working times:', error)
-      }
-    },
-    async getWorkingTimes() {
-      try {
-        const response = await axios.get(`http://localhost:4000/api/workingtimes/` + 1)
-        this.workingTimes = response.data.data
-        this.loading = false
-      } catch (error) {
-        console.error('Error fetching working times:', error)
-      }
-    },
-    formatTime(dateString) {
-      const date = new Date(dateString)
-      return (
-        date.getUTCHours().toString().padStart(2, '0') +
-        ':' +
-        date.getUTCMinutes().toString().padStart(2, '0')
-      )
-    },
-    calculateDuration(start, end) {
-      const startDate = new Date(start)
-      const endDate = new Date(end)
-      const duration = (endDate - startDate) / (1000 * 60 * 60) // Duration in hours
-      return duration.toFixed(2)
-    },
-    calculateTotalDuration(times) {
-      return times
-        .reduce((total, time) => {
-          const startDate = new Date(time.start)
-          const endDate = new Date(time.end)
-          return total + (endDate - startDate) / (1000 * 60 * 60)
-        }, 0)
-        .toFixed(2)
-    },
-    getPeriod(start) {
-      const hour = new Date(start).getHours()
-      return hour < 12 ? 'Morning' : 'Afternoon'
-    },
-    toggleDetails(date) {
-      this.expandedDate = this.expandedDate === date ? null : date
-    }
-  },
-  mounted() {
-    this.getWorkingTimes()
-  }
-}
-</script>
-
-<style scoped>
-/* Ajoutez ici des styles supplémentaires pour personnaliser davantage les listes */
-</style>
