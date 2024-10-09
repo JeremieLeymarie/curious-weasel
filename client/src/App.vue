108b40983530da9a51eb4bd5a65adde609a48e13
Allie <busell_i@etna-alternance.net>
2024-10-09 13:45:17 +0200

    :lipstick: hompage and header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,9 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/Logo_nobg.png" width="200" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <Home />
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
+<script setup lang="ts">
+import Home from './components/Home.vue';
+</script>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Header from './Header.vue';
+</script>
+
+<template>
+    <Header />
+    <h1>Welcome, {{  }}!</h1>
+    <p>Today is {{  }}</p>
+    <hr>
+    <div class="flex">
+        <div class="items-left">
+        <h3>Fast Clocking</h3>
+        <button><a>DECLARE MY ARRIVAL</a></button>
+        <button><a>DECLARE MY DEPARTURE</a></button>
+    </div>
+    <div class="items-right">
+        <h3>Overview</h3>
+        <span><p>You have worked {{ }} this week.</p></span>
+        <span><p>You have {{  }} days off left.</p></span>
+        <button><a>Consult dashboard</a></button>
+    </div>
+    </div>
+    
+</template>
