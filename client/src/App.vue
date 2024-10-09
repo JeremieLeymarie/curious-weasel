02252a1f135ca701996d2479eed83577d5d65857
JeremieLeymarie <jeremie.leymarie@napta.io>
2024-10-09 12:05:16 +0200

    :sparkles: Add basic user CRUD (no create)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
+// import UserComponent from './components/UserComponent.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/Logo_nobg.png" width="200" height="125" />
-
-    <p class="text-emerald-500">Pas de panique à bord, le fun et la vitesse à bord
-      Pas de panique à bord
-      C'est les Ratz, c'est les Ratz qui sont là pour ça
-      Pas de panique à bord (c'est les Ratz), le fun et la vitesse à bord (c'est les Ratz)
-      Les rats ne quittent pas le navire, on vous emmènera à bon port</p>
+    <img alt="Vue logo" class="logo" src="./assets/Logo_nobg.png" width="100" height="125" />
   </header>
   <main>
+    <!-- <UserComponent /> -->
   </main>
 </template>
