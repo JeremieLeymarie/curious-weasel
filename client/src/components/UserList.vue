91e72dcb92289c72908a7074d9782c280107323e
JeremieLeymarie <jeremie.leymarie@napta.io>
2024-10-10 15:03:06 +0200

    :sparkles: Add employees page

--- client/src/components/UserList.vue
+++ client/src/components/UserList.vue
@@ -18,30 +18,32 @@ const tdClass = 'border border-blue-900 p-2'
 </script>
 
 <template>
-  <p class="text-3xl">Employees</p>
-  <hr class="h-1 mt-2 mb-6 bg-[#1343ad] border-0" />
-  <div v-if="users">
-    <table>
-      <thead>
-        <tr>
-          <th
-            :class="`${tdClass} font-bold text-blue-900`"
-            scope="col"
-            v-for="header in headers"
-            :key="header"
-          >
-            {{ capFL(header) }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users" :key="user.id">
-          <th :class="tdClass" scope="row">{{ user.id }}</th>
-          <td :class="tdClass">{{ user.username }}</td>
-          <td :class="tdClass">{{ user.email }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="mx-8 mt-4">
+    <p class="text-3xl">Employees</p>
+    <hr class="h-1 mt-2 mb-6 bg-[#1343ad] border-0" />
+    <div v-if="users">
+      <table>
+        <thead>
+          <tr>
+            <th
+              :class="`${tdClass} font-bold text-blue-900`"
+              scope="col"
+              v-for="header in headers"
+              :key="header"
+            >
+              {{ capFL(header) }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <th :class="tdClass" scope="row">{{ user.id }}</th>
+            <td :class="tdClass">{{ user.username }}</td>
+            <td :class="tdClass">{{ user.email }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else>Loading...</div>
   </div>
-  <div v-else>Loading...</div>
 </template>
