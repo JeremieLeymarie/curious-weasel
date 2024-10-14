a4898367a553073680b835442c08cc1a0cbb39c0
JeremieLeymarie <jeremie.leymarie@napta.io>
2024-10-14 10:51:26 +0200

    :card_file_box: Add minimal seed

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+TimeTracker.Repo.insert!(%TimeTracker.User{
+  username: "admin",
+  email: "admin@admin.com",
+  role: :general_manager
+})
