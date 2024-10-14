50267dd353a6fa965654b423cd40b729e384e684
DylanWeinachter <dylan.weinachter@gmail.com>
2024-10-14 11:22:13 +0200

    :monocle_face::monocle_face: Start of the auth feature from scratch - can u bench 225 ?

---
+++ server/lib/time_tracker/accounts/account.ex
@@ -0,0 +1,22 @@
+defmodule TimeTracker.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "accounts" do
+    field :hash_password, :string
+    field :email, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:email, :hash_password])
+    |> validate_required([:email, :hash_password])
+    |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:hash_password, min: 6)
+  end
+
+end
---
+++ server/test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule TimeTracker.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `TimeTracker.Accounts` context.
+  """
+
+  @doc """
+  Generate a account.
+  """
+  def account_fixture(attrs \\ %{}) do
+    {:ok, account} =
+      attrs
+      |> Enum.into(%{
+        email: "some email",
+        hash_password: "some hash_password"
+      })
+      |> TimeTracker.Accounts.create_account()
+
+    account
+  end
+end
