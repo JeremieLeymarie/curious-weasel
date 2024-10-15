542d2acba97fe6be7caf149ddfd797321cf9414a
Guillaum-th <guillaume.thiebaud@epitech.eu>
2024-10-15 09:49:07 +0200

    add grafana sonarqube prometheus

---
+++ client/src/components/Authentication.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
+import AppInput from './ui/AppInput.vue'
+import AppButton from './ui/AppButton.vue'
+
+const router = useRouter()
+const route = useRoute()
+
+const email = ref(null)
+const password = ref(null)
+async function login() {
+  let data = JSON.stringify({
+    working_time: {
+      email: email.value,
+      password: password.value
+    }
+  })
+  if (password.value != null && email.value != null) {
+    let res = await fetch('http://localhost:4000/api/workingtimes/' + route.params.id, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: data
+    })
+    if (res.status == 200) {
+      router.push(`/`)
+    }
+  } else {
+    console.log('no email or password')
+  }
+}
+</script>
+
+<template>
+  <div class="ml-8">
+    <p class="text-2xl my-4">Connections</p>
+    <hr class="h-1 mb-4 ml-1 mr-16 bg-[#1343ad] border-0" />
+    <div class="flex space-x-10">
+      <div class="p-2">
+        <p>Email</p>
+        <AppInput v-model="email" type="text" name="" id="" class="border-2" />
+      </div>
+      <div class="p-2">
+        <p>Password</p>
+        <AppInput v-model="password" type="password" name="" id="" class="border-2" />
+      </div>
+    </div>
+    <AppButton @click="login"> Submit </AppButton>
+  </div>
+</template>
